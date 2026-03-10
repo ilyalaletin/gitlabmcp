51defecbaff0bdcc1f333590ff8ad2eb1b006498
ilya <laletin.ilya@gmail.com>
2026-03-10 19:17:16 +0300

    feat: add GitLab client wrapper

---
+++ internal/client/client.go
@@ -0,0 +1,14 @@
+package client
+
+import (
+	"github.com/ilya/gitlabmcp/internal/config"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+func New(cfg *config.Config) (*gitlab.Client, error) {
+	opts := []gitlab.ClientOptionFunc{}
+	if cfg.URL != "https://gitlab.com" {
+		opts = append(opts, gitlab.WithBaseURL(cfg.URL))
+	}
+	return gitlab.NewClient(cfg.Token, opts...)
+}
---
+++ internal/client/client_test.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/ilya/gitlabmcp/internal/config"
+)
+
+func TestNew_DefaultURL(t *testing.T) {
+	cfg := &config.Config{
+		Token: "test-token",
+		URL:   "https://gitlab.com",
+	}
+	c, err := New(cfg)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if c == nil {
+		t.Fatal("expected non-nil client")
+	}
+}
+
+func TestNew_CustomURL(t *testing.T) {
+	cfg := &config.Config{
+		Token: "test-token",
+		URL:   "https://gitlab.example.com",
+	}
+	c, err := New(cfg)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if c == nil {
+		t.Fatal("expected non-nil client")
+	}
+}
